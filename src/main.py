791808085767c61d3bc4a7ed7edbc7bcfa1635f2
Chloe <93793296+czjyyds@users.noreply.github.com>
2021-12-16 08:33:07 -0800

    configure cors policy

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 # app/main.py
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from generate import Generate
 
 app = FastAPI(title="GPT2 Story Generator")
+
+# TODO: wildcard '*' domain should not be used in production
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 generation = Generate()
 
 @app.get("/v1/gpt2/generate")
