21de05ddeb32ec972fd0e268d1b37a3ae0dd9487
Austin <95847866+austinl90@users.noreply.github.com>
2022-01-06 16:54:37 -0800

    add cors headers

--- src/api.py
+++ src/api.py
@@ -2,9 +2,20 @@
 
 from fastapi import FastAPI
 from generation.generate import Generate
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="GPT2 Story Generator")
 generation = Generate()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/v1/gpt2/generate")
 def generate(prefix: str):
